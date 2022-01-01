7760a893bba69e7ad46cd45678f8a5f2fb7c3205
vaca <wsmslgh@gmail.com>
2022-01-01 18:17:46 +0800

    Initial Commit

---
+++ asd/asdApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdApp.swift
+//  asd
+//
+//  Created by mac on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct asdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
